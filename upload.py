64b0a1080d60a5d37c77975da022db4a89effe1a
Saeed <jibonrootnext@gmail.com>
2025-08-24 00:40:38 +0600

    24.08.2025

--- upload.py
+++ upload.py
@@ -1,8 +1,13 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
 
+from selenium import webdriver
+#from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+file_path = "C:\\Users\\Khan Gadget\\Desktop\\download.xlsx"
 # chrome driver service
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
@@ -10,9 +15,13 @@ driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/upload-download-test/index.html")
 driver.find_element(By.ID, "downloadButton").click()
 
-# edit the with uploaded file
-
+# uploaded excel file
 file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
-file_input.send_keys("")
+file_input.send_keys(file_path)
 
 
+wait = WebDriverWait(driver, 10)  # explicit wait
+toast_locator = (By.CSS_SELECTOR, ".Toastify__toast-body div:nth-child(2)")
+wait.until(expected_conditions.visibility_of_element_located(toast_locator))
+
+print(driver.find_element(*toast_locator).text)
