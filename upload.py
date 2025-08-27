97425d09ba5972494a1764f5e9f35fbbf07251e3
shadmansaeed <shadmansaeedjibon@gmail.com>
2025-08-28 02:02:39 +0600

    28.08.2025

--- upload.py
+++ upload.py
@@ -7,6 +7,30 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
+import openpyxl
+
+
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.workbook import Workbook
+
+book: Workbook = openpyxl.load_workbook("C:\\Users\\Khan Gadget\\Desktop\\PythonDemo.xlsx")
+
+# Excel active
+sheet: Worksheet = book.active
+Dict = {}
+
+for i in range(1, sheet.max_column+1):
+    if sheet.cell(row=1, column=i).value == "price":
+        Dict["col"] = i
+
+for i in range(1, sheet.max_row+1):
+    for j in range(1, sheet.max_column+1):
+        if sheet.cell(row=i, column=j).value == "Apple":
+            Dict["row"] =i
+
+
+
+
 file_path = "C:\\Users\\Khan Gadget\\Desktop\\download.xlsx"
 fruit_name = "Apple"
 # chrome driver service
@@ -17,6 +41,9 @@ driver.get("https://rahulshettyacademy.com/upload-download-test/index.html")
 driver.find_element(By.ID, "downloadButton").click()
 
 # uploaded excel file
+
+sheet.cell(row=Dict["row"], column=Dict["col"]).value = "500"
+book.save(file_path)
 file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
 file_input.send_keys(file_path)
 
