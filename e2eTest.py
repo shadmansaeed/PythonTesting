d4279802519ec1d3c2aa986258b2b1acfd49274f
shadmansaeed <shadmansaeedjibon@gmail.com>
2025-09-01 01:34:51 +0600

    01.09.2025

--- e2eTest.py
+++ e2eTest.py
@@ -19,7 +19,10 @@ products= driver.find_elements(By.XPATH,"//div[@class='card h-100']")
 for product in products:
     productName= product.find_element(By.XPATH,"div/h4/a").text
     if productName == "Blackberry":
+        product.find_element(By.XPATH, "div/button").click()
 
+
+driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
 
 
 time.sleep(5)
